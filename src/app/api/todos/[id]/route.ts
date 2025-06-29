ba05eb6d16a12ac251b333920a74121e5787284c
mrskg0438 <gaudshwetkamal0438@gmail.com>
2025-06-29 20:06:57 +0530

    some chnages

--- src/app/api/todos/[id]/route.ts
+++ src/app/api/todos/[id]/route.ts
@@ -1,27 +1,27 @@
 import { connectDB } from "@/config/db";
 import { Todo } from "@/models/Todo";
 import { getCurrentUser } from "@/utils/auth";
-import { NextResponse } from "next/server";
-
-export async function PUT(req: Request, { params }: { params: { id: string } }) {
+import { NextResponse, NextRequest } from "next/server";
+type tParams = Promise<{ id: string }>;
+export async function PUT(req: NextRequest, context: { params: tParams } ) {
     const { title, status } = await req.json();
     const user = await getCurrentUser();
-
+    const { id } = await context.params
     if (!user) {
         return new Response(JSON.stringify({ message: 'Unauthorized' }), { status: 401 });
     }
     await connectDB();
-    const updated = await Todo.findByIdAndUpdate(params.id, { title, status }, { new: true });
+    const updated = await Todo.findByIdAndUpdate(id, { title, status }, { new: true });
     return NextResponse.json(updated);
 }
 
-export async function DELETE(_: Request, { params }: { params: { id: string } }) {
+export async function DELETE(_: NextRequest, context: { params: tParams }  ) {
     const user = await getCurrentUser();
-
+    const { id } = await context.params
     if (!user) {
         return new Response(JSON.stringify({ message: 'Unauthorized' }), { status: 401 });
     }
     await connectDB();
-    await Todo.findByIdAndDelete(params.id);
+    await Todo.findByIdAndDelete(id);
     return NextResponse.json({ message: "Deleted" });
 }
